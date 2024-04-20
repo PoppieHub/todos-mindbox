a79200ab59550cb51e0b335d8c3cc145a53f711b
PoppieHub <hoolm-2012@yandex.ri>
2024-04-20 21:14:43 +0500

    Fixed incorrect behavior isСompiled with reset in every unselected object

--- app/client/src/containers/InputTask.tsx
+++ app/client/src/containers/InputTask.tsx
@@ -5,35 +5,20 @@ import { InputTask as InputTaskComponent } from '../components';
 
 const InputTask: React.FC<InputTaskProps> = ({ task, onClickTask }) => {
     const [flag, setFlag] = useState(task.isCompleted);
-    const [tasks, setTasks] = useState<TaskProps[]>(getCollectionTasks() ?? []);
 
     useEffect(() => {
-        const updatedTasks = tasks.map(t => (t.id === task.id ? { ...t, isCompleted: flag } : t));
-        // Проверяем, изменились ли задачи, прежде чем обновлять состояние
-        if (!tasksAreEqual(updatedTasks, tasks)) {
-            setTasks(updatedTasks);
-        }
-    }, [flag, task, tasks]);
+        const updateTaskInCollection = (updatedTask: TaskProps) => {
+            const updatedTasks = getCollectionTasks().map(t => (t.id === updatedTask.id ? updatedTask : t));
+            setCollectionTasks(updatedTasks);
+        };
 
-    useEffect(() => {
-        const filteredTasks = tasks.filter(t => !t.isCompleted);
+        const updatedTask = { ...task, isCompleted: flag };
+        updateTaskInCollection(updatedTask);
+
+        // Вызываем onClickTask с отфильтрованными задачами (невыполненными)
+        const filteredTasks = getCollectionTasks().filter(t => !t.isCompleted);
         onClickTask(filteredTasks);
-        // Вызываем setCollectionTasks с актуальным значением tasks
-        setCollectionTasks(tasks);
-    }, [tasks, onClickTask]);
-
-    // Функция для проверки равенства массивов задач
-    const tasksAreEqual = (tasks1: TaskProps[], tasks2: TaskProps[]): boolean => {
-        if (tasks1.length !== tasks2.length) {
-            return false;
-        }
-        for (let i = 0; i < tasks1.length; i++) {
-            if (tasks1[i].id !== tasks2[i].id || tasks1[i].isCompleted !== tasks2[i].isCompleted) {
-                return false;
-            }
-        }
-        return true;
-    };
+    }, [flag, task, onClickTask]);
 
     return (
         <InputTaskComponent
--- app/client/src/models/InputTaskProps.ts
+++ app/client/src/models/InputTaskProps.ts
@@ -1,5 +1,5 @@
 import {onClickTask, TaskProps} from "./index";
 
-export default interface TaskListProps extends onClickTask {
+export default interface InputTaskProps extends onClickTask {
     task: TaskProps;
 }
